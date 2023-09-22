92fbc3e360fd5d84278a0bba37b0d47e2035a782
unknown <motaviny140@gmail.com>
2023-09-22 10:04:48 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => console.log(r));
+async function buscaEndereco(cep) {
+  let mensagemErro = document.querySelector("#erro");
+  mensagemErro.innerHTML = "";
+  try {
+    let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    let consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro) {
+      throw Error("CEP não existente!");
+    }
+    let logradouro = document.querySelector("#endereco");
+    let cidade = document.querySelector("#cidade");
+    let estado = document.querySelector("#estado");
+    let bairro = document.querySelector("#bairro");
 
-console.log(consultaCEP);
+    cidade.value = consultaCEPConvertida.localidade;
+    estado.value = consultaCEPConvertida.uf;
+    logradouro.value = consultaCEPConvertida.logradouro;
+    bairro.value = consultaCEPConvertida.bairro;
+
+    console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
+  } catch (erro) {
+    mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
+    console.log(erro);
+  }
+}
+
+let cep = document.querySelector("#cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+
+// Como ter varias promisses de uma vez
+// let ceps = ['01001000', '01001001']
+// let conjuntoCeps = ceps.map(valores => buscaEndereco(valores))
+// Promise.all(conjuntoCeps).then(respostas => console.log(respostas))
